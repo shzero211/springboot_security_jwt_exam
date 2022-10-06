1e3c900b9a34414f431ea597cf660f8236d638d5
SangHun <shzero211@naver.com>
2022-10-06 13:39:44 +0900

    ResponseEntity로 header 추가

--- src/main/java/com/ll/exam/spring_security_jwt_exam/member/MemberController.java
+++ src/main/java/com/ll/exam/spring_security_jwt_exam/member/MemberController.java
@@ -1,13 +1,20 @@
 package com.ll.exam.spring_security_jwt_exam.member;
 
-import org.springframework.stereotype.Controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
-    public String join(@RequestBody LoginDto loginDto){
-        return "usernaeme : %s , password : %s".formatted(loginDto.getUsername(),loginDto.getPassword());
+    public ResponseEntity<String> join(@RequestBody LoginDto loginDto){
+        HttpHeaders headers=new HttpHeaders();
+        headers.set("Authentication","JWT키");
+       String body= "usernaeme : %s , password : %s".formatted(loginDto.getUsername(),loginDto.getPassword());
+       return new ResponseEntity<>(body,headers, HttpStatus.OK);
     }
 }
