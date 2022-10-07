8761021222f2139e17da49c7ef4c3dc99b98ab11
SangHun <shzero211@naver.com>
2022-10-07 13:38:15 +0900

    로그인과 회원가입 엔드 포인트 제외하고, 전부 인증필요, 필터추가

--- src/main/java/com/ll/exam/spring_security_jwt_exam/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/spring_security_jwt_exam/security/SecurityConfig.java
@@ -6,20 +6,30 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws  Exception{
         http
+                .authorizeRequests(
+                        authorizeRequests->authorizeRequests
+                                .antMatchers("/member/login","/member/join")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
                 .cors().disable()
                 .csrf().disable()
                 .httpBasic().disable()
                 .formLogin().disable()
-                .sessionManagement(sessionManagement->sessionManagement.sessionCreationPolicy(STATELESS));
+                .sessionManagement(sessionManagement->sessionManagement.sessionCreationPolicy(STATELESS)).addFilterBefore(
+                        jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
